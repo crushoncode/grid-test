64a875c49c5c77d1bdb29824e5b16c8ba4f1257a
Serina Ko <serinako08@gmail.com>
2018-09-18 21:18:41 +1000

    put background image to the left side and grid on the top of it

--- src/App.js
+++ src/App.js
@@ -8,37 +8,35 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <div className="wrapper">
-          <img
-            className="bgImage"
-            src={business}
-            alt="logo"
-            width="300"
-            height="300"
-          />
-          <div className="box d">
-            <div className="box e">
-              <div className="headTitle">Hey grid,</div>
-              <dic className="subTitle">See you tmr</dic>
-            </div>
-            <div className="box f">
-              <img
-                className="indiLogo"
-                src={inglogo}
-                alt="logo"
-                width="100"
-                height="60"
-              />
-            </div>
-            <div className="box g">
-              <img
-                className="indiLogo"
-                src={google}
-                alt="logo"
-                width="100"
-                height="60"
-              />
-            </div>
+        <img
+          className="backgroundImage"
+          src={business}
+          alt="logo"
+          width="100"
+          height="60"
+        />
+        <div className="box d">
+          <div className="box e">
+            <div className="headTitle">Hey grid,</div>
+            <dic className="subTitle">See you tmr</dic>
+          </div>
+          <div className="box f">
+            <img
+              className="indiLogo"
+              src={inglogo}
+              alt="logo"
+              width="100"
+              height="60"
+            />
+          </div>
+          <div className="box g">
+            <img
+              className="indiLogo"
+              src={google}
+              alt="logo"
+              width="100"
+              height="60"
+            />
           </div>
         </div>
       </Fragment>
