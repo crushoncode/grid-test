83882bea134b54ee07861a5036d9f0dcf58354da
Serina Ko <serinako08@gmail.com>
2018-09-18 20:19:55 +1000

    add text and logo into grid boxes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import inglogo from './test1.png';
+import google from './test2.png';
 
 class App extends Component {
   render() {
     return (
-      <div class="wrapper">
-        <div class="box a">A</div>
-        <div class="box b">B</div>
-        <div class="box c">C</div>
-        <div class="box d">
-          <div class="box e">E</div>
-          <div class="box f">F</div>
-          <div class="box g">G</div>
+      <div className="wrapper">
+        <div className="box c">C</div>
+        <div className="box d">
+          <div className="box e">
+            <div className="headTitle">Hey grid,</div>
+            <dic className="subTitle">See you tmr</dic>
+          </div>
+          <div className="box f">
+            <img src={inglogo} alt="logo" width="100" height="60" />
+          </div>
+          <div className="box g">
+            <img src={google} alt="logo" width="100" height="60" />
+          </div>
         </div>
       </div>
     );
