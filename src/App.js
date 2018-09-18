dd0e2202e150b607913ab679f200a7e1b3150f25
Serina Ko <serinako08@gmail.com>
2018-09-18 20:40:32 +1000

    add background image

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import inglogo from './test1.png';
 import google from './test2.png';
+import business from './Business.jpg';
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <div className="box c">C</div>
-        <div className="box d">
-          <div className="box e">
-            <div className="headTitle">Hey grid,</div>
-            <dic className="subTitle">See you tmr</dic>
-          </div>
-          <div className="box f">
-            <img src={inglogo} alt="logo" width="100" height="60" />
-          </div>
-          <div className="box g">
-            <img src={google} alt="logo" width="100" height="60" />
+      <Fragment>
+        <div className="wrapper">
+          <img
+            className="bgImage"
+            src={business}
+            alt="logo"
+            width="300"
+            height="300"
+          />
+          <div className="box d">
+            <div className="box e">
+              <div className="headTitle">Hey grid,</div>
+              <dic className="subTitle">See you tmr</dic>
+            </div>
+            <div className="box f">
+              <img
+                className="indiLogo"
+                src={inglogo}
+                alt="logo"
+                width="100"
+                height="60"
+              />
+            </div>
+            <div className="box g">
+              <img
+                className="indiLogo"
+                src={google}
+                alt="logo"
+                width="100"
+                height="60"
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </Fragment>
     );
   }
 }
