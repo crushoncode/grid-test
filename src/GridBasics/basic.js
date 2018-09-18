c9a75bf838089479d783df7301e43e47fce93c6d
Serina Ko <serinako08@gmail.com>
2018-09-18 19:47:04 +1000

    add folders

---
+++ src/GridBasics/basic.js
@@ -0,0 +1,7 @@
+<div className="grid">
+  <div className="title">Title</div>
+  {/* <div className="header">Header</div> */}
+  {/* <div className="sidebar">Sidebar</div> */}
+  <div className="content">Content</div>
+  <div className="footer">Footer</div>
+</div>;
